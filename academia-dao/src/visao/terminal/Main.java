feec2af9a41aa12dfb9ad76821b505f89f3a168d
Gilson Levi <117370090+gilsonlevi@users.noreply.github.com>
2023-04-26 17:22:27 -0300

    Update Main.java

--- academia-dao/src/visao/terminal/Main.java
+++ academia-dao/src/visao/terminal/Main.java
@@ -7,17 +7,15 @@ public class Main {
 		
 		Scanner teclado = new Scanner(System.in);
 		while(true) {
-			System.out.println("----------------------------");
 			System.out.println("Bem vindo");
 			System.out.println("0. Sair do programa");
 			System.out.println("1. Adicionar funcionario");
 			System.out.println("2. Buscar funcionario");
 			System.out.println("3. Excluir funcionario");
 			System.out.println("4. Ver todos os funcionarios");
-			System.out.println("Digite um opção: ");
+			System.out.println("Digite um opÃ§Ã£o: ");
 			int resp = teclado.nextInt();
 			teclado.nextLine();
-			System.out.println("----------------------------");
 			if(resp == 0) {
 				System.out.println("----------------------------");
 				System.out.println("Saindo do programa...");
